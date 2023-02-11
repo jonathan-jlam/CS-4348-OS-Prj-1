47ba9153f08760d6551e1d676263f0526ba3e7c0
Jonathan Lam <jonathan.kw.lam@gmail.com>
2023-02-10 20:24:27 -0600

    removed unnecessary stuff

--- main.c
+++ main.c
@@ -7,40 +7,18 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-#define MAXLEN 100
+
 
 int toMem[2];
 int toCPU[2];
 
 
-
 void mem() {
 
     char message[] = "HELLO FROM THE PARENT!";
-	//printf("This is the file got by mem: %s\n", filename);
     write(toCPU[1], message, 100);
 	read(toMem[0], message, 100);
 	printf("The parent got BACK: %s\n", message);
-	//int memory[2000];
-
-/*
-	FILE *fp = fopen(filename, "r");
-
-	
-	if (!fp) {
-		fprintf(stderr, "Filename %s cannot be opened for reading.\n", filename);
-		printf("%s", filename);
-		exit(2);
-	}
-
-	printf("Opened %s for reading.\n", filename);
-	char line[MAXLEN];
-	while(fgets(line, MAXLEN, fp)) {
-		printf("%s\n", line);
-	}
-
-	fclose(fp);
-	*/
 }
 
 
@@ -74,10 +52,6 @@ int main(int argc, char* argv[]) {
 			break;
 
         default:           
-			//char filename[100];
-		//	printf("%s", argv[1]);
-		//	strcpy(filename, argv[1]);
-           // mem(filename);
 		   mem();
 			
 			

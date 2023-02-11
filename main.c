10189b26ff1ad1b8b26444918aece2688dcbdfbb
Jonathan Lam <jonathan.kw.lam@gmail.com>
2023-02-10 20:45:48 -0600

    fixed segfault

--- main.c
+++ main.c
@@ -15,8 +15,11 @@ int toCPU[2];
 
 void mem() {
 
-    char message[] = "HELLO FROM THE PARENT!";
+    char message[100];
+	sprintf(message, "Hello from the parent!\n");
     write(toCPU[1], message, 100);
+
+	//char message2[100];
 	read(toMem[0], message, 100);
 	printf("The parent got BACK: %s\n", message);
 }
@@ -37,11 +40,13 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+/*
 	if (argc < 2) {
 		fprintf(stderr, "Improper Usage: <program> <filename>");
 		exit(1);
 	}
-
+	*/
+/*
     switch(fork()) {
         case -1: 
             write(STDERR_FILENO, "Fork failed\n", 12);
@@ -57,11 +62,25 @@ int main(int argc, char* argv[]) {
 			
     }
 
+*/
 
 
+int h = fork();
 
-   
+if (h == -1) {
+	printf("Error with fork.\n");
+	exit(1);
+}
 
+if (h == 0) {
+	printf("child pid: %d", getpid());
+	CPU();
+} else {
 
+	printf("Parent pid: %d", getpid());
+	mem();
+}
+
+printf("reached here: %d\n", getpid());
    
 }

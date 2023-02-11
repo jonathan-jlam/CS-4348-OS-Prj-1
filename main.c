afec9cd6b94ff1e92bd023edb1a945fdadddfb07
Jonathan Lam <jkl200002@cslinux1.utdallas.edu>
2023-02-10 20:08:27 -0600

    new additions

--- main.c
+++ main.c
@@ -7,52 +7,62 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+#define MAXLEN 100
 
 int toMem[2];
 int toCPU[2];
 
 
 
-int mem() {
-    close(1);
-    dup(toCPU[1]);
-
-    close(0);
-    dup(toMem[0]);
-
-    for (int i = 0; i <= 1; i++) {
-        close(toCPU[i]);
-        close(toMem[i]);
-    }
+void mem() {
 
     char message[] = "HELLO FROM THE PARENT!";
-    write(toCPU[1], "%s", message);
+	//printf("This is the file got by mem: %s\n", filename);
+    write(toCPU[1], message, 100);
+	read(toMem[0], message, 100);
+	printf("The parent got BACK: %s\n", message);
+	//int memory[2000];
+
+/*
+	FILE *fp = fopen(filename, "r");
+
+	
+	if (!fp) {
+		fprintf(stderr, "Filename %s cannot be opened for reading.\n", filename);
+		printf("%s", filename);
+		exit(2);
+	}
+
+	printf("Opened %s for reading.\n", filename);
+	char line[MAXLEN];
+	while(fgets(line, MAXLEN, fp)) {
+		printf("%s\n", line);
+	}
+
+	fclose(fp);
+	*/
 }
 
 
-int CPU() {
-    close(0);
-    dup(toCPU[0]);
-
-    close(1);
-    dup(toMem[1]);
-
-    for (int i = 0; i <= 1; i++) {
-        close(toCPU[i]);
-        close(toMem[i]);
-    }
+void CPU() {
 
     char receive[100];
-    read(toCPU[0], &receive, 100);
-    sprintf(stderr, "%sNEWNEWNENW", receive);
+    read(toCPU[0], receive, 100);
+    fprintf(stderr, "The CPU got: %s\n", receive);
+	write(toMem[1], "Hello from the child!", 100);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     
     if (pipe(toMem) < 0 || pipe(toCPU) < 0) {
         write(STDERR_FILENO, "Pipe failed\n", 12);
         exit(1);
     }
+
+	if (argc < 2) {
+		fprintf(stderr, "Improper Usage: <program> <filename>");
+		exit(1);
+	}
 
     switch(fork()) {
         case -1: 
@@ -60,9 +70,17 @@ int main() {
             exit(2);
 
         case 0:
-            mem();
-        default:
-            CPU();
+			CPU();
+			break;
+
+        default:           
+			//char filename[100];
+		//	printf("%s", argv[1]);
+		//	strcpy(filename, argv[1]);
+           // mem(filename);
+		   mem();
+			
+			
     }
 
 
